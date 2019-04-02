a6dacd71c15497e337cf13cd9dc421631749d929
Georgie Morales <georgemoralesjr@hotmail.com>
2019-04-02 13:46:20 -0400

    Add files via upload
    Light.java

---
+++ Light.java
@@ -0,0 +1,41 @@
+package test;
+
+public class Light {
+
+	boolean on;
+	int floorNumber;
+	
+	//constructor
+	public Light(int number) { //Light class definition
+		
+		floorNumber=number;
+		on=false;
+		
+			
+	
+			System.out.println("floor"+floorNumber+"light constructed");
+		 
+		
+		
+
+	}
+
+//turn light on
+public void turnOn() { //if light not on, turn it on
+	if(!on) {
+		on=true;
+		System.out.println("floor"+floorNumber+"light turns on");
+		
+	}// end if
+	
+}//end function turnOn
+
+public void turnOff() {
+	if(on) {
+		on=false;
+		System.out.println("floor"+floorNumber+"light turns off");
+	}//end if
+	
+}//end function turnOff
+
+}
