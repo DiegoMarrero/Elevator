d98bea9522bc786950f88f722c4590f7bbc86642
Janiel Martell <martell.janiel@gmail.com>
2019-04-05 13:43:54 -0400

    Add Bell Class

---
+++ Bell.java
@@ -0,0 +1,9 @@
+public class Bell {
+    public Bell() {
+        System.out.println("bell created");
+    }
+
+    public void ringBell() {
+        System.out.println("elevator rings its bell");
+    }
+}
