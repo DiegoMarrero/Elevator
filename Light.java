80dd554506c5099dc895492313fb0e729be00572
Janiel Martell <martell.janiel@gmail.com>
2019-04-06 17:37:30 -0400

    remove package and add formatting

--- Light.java
+++ Light.java
@@ -1,41 +1,29 @@
-package test;
-
 public class Light {
 
-	boolean on;
-	int floorNumber;
-	
-	//constructor
-	public Light(int number) { //Light class definition
-		
-		floorNumber=number;
-		on=false;
-		
-			
-	
-			System.out.println("floor"+floorNumber+"light constructed");
-		 
-		
-		
+	private boolean on;
+	private final int floorNumber;
 
+	// constructor
+	public Light(int number) { // Light class definition
+
+		floorNumber = number;
+		on = false;
+
+		System.out.println("floor " + floorNumber + " light constructed");
 	}
 
-//turn light on
-public void turnOn() { //if light not on, turn it on
-	if(!on) {
-		on=true;
-		System.out.println("floor"+floorNumber+"light turns on");
-		
-	}// end if
-	
-}//end function turnOn
+	// turn light on
+	public void turnOn() { // if light not on, turn it on
+		if (!on) {
+			on = true;
+			System.out.println("floor " + floorNumber + " light turns on");
+		} // end if
+	}// end function turnOn
 
-public void turnOff() {
-	if(on) {
-		on=false;
-		System.out.println("floor"+floorNumber+"light turns off");
-	}//end if
-	
-}//end function turnOff
-
+	public void turnOff() {
+		if (on) {
+			on = false;
+			System.out.println("floor " + floorNumber + " light turns off");
+		} // end if
+	}// end function turnOff
 }
