2045c9689c0bef0ab3a55ceb4da0812fe9932326
Janiel Martell <martell.janiel@gmail.com>
2019-04-06 17:14:00 -0400

    Update format

--- Door.java
+++ Door.java
@@ -1,54 +1,45 @@
-package test;
-
-//import Door;
-//import Person;
-//import Floor
-//import Elevator
-
 public class Door {
-	
-boolean open;
 
-//constructor
-public Door() {
-	
-	open=false; //initialize open to false
-	
-	{
-		System.out.println("door constructed");
-	}
-	
-}//end Door constructor
+	boolean open;
 
-//open the door
-public void openDoor(Person passengerPtr,Person nextPassengerPtr, Floor currentFloor,Elevator elevator)
-{
-	if(!open) { // if door is not open, open door
-		open=true;
-		
-		System.out.println("elevator opens its door on floor"+currentFloor.getNumber());
-		
-		//if passenger is in elevator, tell person to leave
-		if(passengerPtr !=0) {
-			passengerPtr.exitElevator(currentFloor, elevator);
-			
-		}//end if
-		
-		//if passenger waiting to enter elevator
-		// tell passenger to enter
-		if(nextPassengerPtr !=0)
-			nextPassengerPtr.enterElevator(elevator,currentFloor);
-	}// end outer if
-}// end function openDoor
+	// constructor
+	public Door() {
 
-//close the door
-public void closeDoor(Floor currentFloor)
-{
-if(open) { //if door is open, close door
-	open=false;
-	System.out.println("elevator closes its door on floor"+currentFloor.getNumber());
-}//end if
+		open = false; // initialize open to false
 
-}//end function closeDoor
+		{
+			System.out.println("door constructed");
+		}
+
+	}// end Door constructor
+
+	// open the door
+	public void openDoor(Person passengerPtr, Person nextPassengerPtr, Floor currentFloor, Elevator elevator) {
+		if (!open) { // if door is not open, open door
+			open = true;
+
+			System.out.println("elevator opens its door on floor" + currentFloor.getNumber());
+
+			// if passenger is in elevator, tell person to leave
+			if (passengerPtr != 0) {
+				passengerPtr.exitElevator(currentFloor, elevator);
+
+			} // end if
+
+			// if passenger waiting to enter elevator
+			// tell passenger to enter
+			if (nextPassengerPtr != 0)
+				nextPassengerPtr.enterElevator(elevator, currentFloor);
+		} // end outer if
+	}// end function openDoor
+
+	// close the door
+	public void closeDoor(Floor currentFloor) {
+		if (open) { // if door is open, close door
+			open = false;
+			System.out.println("elevator closes its door on floor" + currentFloor.getNumber());
+		} // end if
+
+	}// end function closeDoor
 
 }
