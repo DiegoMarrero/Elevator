662ae7460420c8c3e7e83b1102aaef56bcdc339a
Janiel Martell <martell.janiel@gmail.com>
2019-04-06 17:07:48 -0400

    Add Floor Class

---
+++ Floor.java
@@ -0,0 +1,49 @@
+public class Floor {
+    static final int Floor1 = 1;
+    static final int Floor2 = 2;
+
+    FloorButton floorButton;
+
+    private final int floorNumber;
+    private Elevator elevator;
+    private Person occupant;
+    private Light light;
+
+    public Floor(int number, Elevator elevator) {
+        floorButton = new FloorButton(number, elevator);
+        floorNumber = number;
+        this.elevator = elevator;
+        occupant = null;
+        light = new Light(0);
+    }
+
+    public boolean isOccupied() {
+        return occupant == null ? false : true;
+    }
+
+    public int getNumber() {
+        return floorNumber;
+    }
+
+    public void personArrives(Person person) {
+        occupant = person;
+    }
+
+    public Person elevatorArrived() {
+        System.out.println("floor " + floorNumber + " resets its button");
+
+        floorButton.resetButton();
+        light.turnOn();
+
+        return occupant;
+    }
+
+    public void elevatorLeaving() {
+        light.turnOff();
+    }
+
+    public void personBoardingElevator() {
+        occupant = null;
+    }
+
+}
