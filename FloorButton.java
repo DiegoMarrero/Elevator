b2bc9c919bd6b8810f0a414a727058f1d1ab9b9d
Janiel Martell <martell.janiel@gmail.com>
2019-04-06 17:43:46 -0400

    Fix ALL THE ERRORS

--- FloorButton.java
+++ FloorButton.java
@@ -1,31 +1,30 @@
-public class floorButton {
-	
-	int floor, floorNumber, elevatorRef;
-	boolean pressed;
-	
-	//constructor
-	public floorButton(floor, Elevator elevatorHandle)
-	floorNumber=floor;
-	pressed=false;
-	elevatorRef=elevatorHandle;
-	{
-		System.out.println(dlooeNumber+"button constructed");
-	}//end FloorButton constructor
-	
-	public void pressButton()
-	{
-		pressed=true;
-		System.out.println("floor"+floorNumber+"button summons elevator");
-		
-		//call elevator to this floor
-		
-		elevatorRef.summonElevator(floorNumber);
-	}//end function pressButton
-	
-	//reset button
-	
-	public void resetButton()
-	{
-		pressed=false;
-	}//end function resetButton
+public class FloorButton {
+
+	private final int _floorNumber;
+	private boolean _pressed;
+	private Elevator _elevator;
+
+	// constructor
+	public FloorButton(int floor, Elevator elevator) {
+		_floorNumber = floor;
+		_pressed = false;
+		_elevator = elevator;
+
+		System.out.println("floor " + _floorNumber + " button constructed");
+	}// end FloorButton constructor
+
+	public void pressButton() {
+		_pressed = true;
+		System.out.println("floor " + _floorNumber + " button summons elevator");
+
+		// call elevator to this floor
+
+		_elevator.summonElevator(_floorNumber);
+	}// end function pressButton
+
+	// reset button
+
+	public void resetButton() {
+		_pressed = false;
+	}// end function resetButton
 }
