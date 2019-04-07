d8bccade15844e073dc2446175d60e8c869b3ec7
Janiel Martell <martell.janiel@gmail.com>
2019-04-07 02:30:59 -0400

    refactor

--- Light.java
+++ Light.java
@@ -4,26 +4,25 @@ public class Light {
 	private final int floorNumber;
 
 	// constructor
-	public Light(int number) { // Light class definition
+	public Light(int floorNumber) {
+		this.floorNumber = floorNumber;
 
-		floorNumber = number;
-		on = false;
-
-		System.out.println("floor " + floorNumber + " light constructed");
+		System.out.println("floor " + this.floorNumber + " light constructed");
 	}
 
 	// turn light on
-	public void turnOn() { // if light not on, turn it on
-		if (!on) {
-			on = true;
-			System.out.println("floor " + floorNumber + " light turns on");
+	public void turnOn() {
+		// if light not on, turn it on
+		if (!this.on) {
+			this.on = true;
+			System.out.println("floor " + this.floorNumber + " light turns on");
 		} // end if
 	}// end function turnOn
 
 	public void turnOff() {
-		if (on) {
-			on = false;
-			System.out.println("floor " + floorNumber + " light turns off");
+		if (this.on) {
+			this.on = false;
+			System.out.println("floor " + this.floorNumber + " light turns off");
 		} // end if
 	}// end function turnOff
 }
