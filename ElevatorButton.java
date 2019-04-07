1c9521d6431fb1a2c99d9fc8cab1b54f0b304b94
Janiel Martell <martell.janiel@gmail.com>
2019-04-06 22:19:29 -0400

    refactor

--- ElevatorButton.java
+++ ElevatorButton.java
@@ -1,20 +1,20 @@
 public class ElevatorButton {
-    private boolean pressed;
-    private Elevator elevator;
+    private boolean _pressed;
+    private Elevator _elevator;
 
     public ElevatorButton(Elevator elevator) {
-        this.pressed = false;
-        this.elevator = elevator;
+        _pressed = false;
+        _elevator = elevator;
     }
 
     public void pressButton() {
-        this.pressed = true;
+        _pressed = true;
         System.out.println("elevator button tells elevator to prepare to leave");
 
-        this.elevator.prepareToLeave(true);
+        _elevator.prepareToLeave(true);
     }
 
     public void resetButton() {
-        this.pressed = false;
+        _pressed = false;
     }
 }
